d41d7a9b7624242eb17545ce2d66194b06d521eb
Julien Maulny <doubjulien@hotmail.fr>
2016-07-24 14:25:34 +0200

    abstract config, add shortcut method at guzzle abstraction level

---
+++ tests/Version/Guzzle5Test.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\CanalTP\AbstractGuzzle;
+
+use CanalTP\AbstractGuzzle\GuzzleFactory;
+use \Mockery as m;
+
+class Guzzle5Test extends \PHPUnit_Framework_TestCase
+{
+    protected function setup()
+    {
+        if (5 !== GuzzleFactory::detectGuzzleVersion()) {
+            $this->markTestSkipped('test for version 5 only');
+        }
+    }
+
+    public function testCreateGuzzleReturnsInitializedInstanceOfAbstractGuzzle()
+    {
+        $baseUri = 'http://my-base-url.tld';
+        $guzzle = GuzzleFactory::createGuzzle($baseUri);
+
+        $this->assertInstanceOf('CanalTP\\AbstractGuzzle\\Version\\Guzzle5', $guzzle);
+        $this->assertEquals($baseUri, $guzzle->getBaseUri());
+    }
+}
